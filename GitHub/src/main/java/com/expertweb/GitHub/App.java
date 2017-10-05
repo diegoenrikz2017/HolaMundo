e9162d3bb1abcaa0b61a1b3fe6a89a79a4fc3923
zion <denriquez@expertweb.com.ec>
2017-10-05 12:55:31 -0500

    comit 2

--- GitHub/src/main/java/com/expertweb/GitHub/App.java
+++ GitHub/src/main/java/com/expertweb/GitHub/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	//primer desarroollador
+    	//segundo desarrollador
         System.out.println( "Hello World!" );
         
         String [] arreglo = {"hola","mundo","expertweb","test"};
