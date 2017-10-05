eb2e0b7754cabae88dfc3a10510d5f17805a62ce
zion <denriquez@expertweb.com.ec>
2017-10-05 12:32:39 -0500

    Git Hub

---
+++ GitHub/src/main/java/com/expertweb/GitHub/App.java
@@ -0,0 +1,16 @@
+package com.expertweb.GitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//primer desarroollador
+        System.out.println( "Hello World!" );
+        
+        String [] arreglo = {"hola","mundo"};
+    }
+}
