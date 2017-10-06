5d1e603783124cf3b52bbd21ba4688a5b3fbda61
zion <denriquez@expertweb.com.ec>
2017-10-06 11:31:19 -0500

    Entidad Persona

---
+++ GitHub/src/main/java/com/expertweb/GitHub/Persona.java
@@ -0,0 +1,30 @@
+package com.expertweb.GitHub;
+
+public class Persona {
+	
+	int id;
+	String nombre;
+	String apellido;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+
+}
